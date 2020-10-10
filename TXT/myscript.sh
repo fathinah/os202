1bfb34fd962f3a78a9075ecff635a594421853a0
Fathinah Asma Izzati <fathinah@DELETE.vlsm.org>
2020-10-10 23:29:12 +0700

    Push week03 assignment

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
-# Fathinah
-# PALSU SEKALI UBAH
+# Mon Sep 28 22:03:27 WIB 2020
+# Tue 29 Sep 2020 11:02:39 AM WIB
+# TIDAK PALSU
 
-FILES="mylog.txt mypubkey.txt myrank.txt myscript.sh"
+FILES="my*.txt my*.sh"
 SHA="SHA256SUM"
 
 rm -f $SHA $SHA.asc
@@ -20,3 +20,5 @@ echo "gpg --verify $SHA.asc $SHA"
 gpg --verify $SHA.asc $SHA
 
 exit 0
+
+# Mon Sep 28 21:05:04 WIB 2020
