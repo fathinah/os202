a0d5adf8f1d792dc20ff16879e0d93073922f50a
Fathinah Asma Izzati <fathinah@DELETE.vlsm.org>
2020-10-19 08:52:59 +0700

    OS202 fathinah

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -1,10 +1,9 @@
-# Mon Sep 28 22:03:27 WIB 2020
-# Tue 29 Sep 2020 11:02:39 AM WIB
-# TIDAK PALSU
+#!/bin/bash
 
 FILES="my*.txt my*.sh"
 SHA="SHA256SUM"
 
+echo "rm -f $SHA $SHA.asc"
 rm -f $SHA $SHA.asc
 
 echo "sha256sum $FILES > $SHA"
@@ -21,4 +20,3 @@ gpg --verify $SHA.asc $SHA
 
 exit 0
 
-# Mon Sep 28 21:05:04 WIB 2020
